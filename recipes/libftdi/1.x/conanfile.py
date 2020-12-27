9f81c70673e4725cd1e075383e96c771f0b93624
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-12-27 03:39:56 +0100

    libftdi1: fix licenses + add patches (trying to get MSVC working, but failed)

--- recipes/libftdi/1.x/conanfile.py
+++ recipes/libftdi/1.x/conanfile.py
@@ -3,22 +3,28 @@ from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
 
 
-class LibFtdiConan(ConanFile):
+required_conan_version = ">=1.29.1"
+
+
+class LibFtdi1Conan(ConanFile):
     name = "libftdi"
     description = "A library to talk to FTDI chips"
-    license = ["GNU LGPL v2.1"]
+    license = "LGPL-2.0-only", "GPLv2-or-later"
     topics = ("conan", "libftdi1")
     homepage = "https://www.intra2net.com/en/developer/libftdi/"
     url = "https://github.com/conan-io/conan-center-index"
-    exports_sources = ["CMakeLists.txt"]
+    exports_sources = "CMakeLists.txt", "patches/*"
     generators = "cmake", "cmake_find_package", "pkg_config"
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-    requires = (
-            "libusb/1.0.23",
-            "boost/1.74.0"
-            )
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
     _cmake = None
 
     @property
@@ -35,67 +41,61 @@ class LibFtdiConan(ConanFile):
             del self.options.fPIC
 
     def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
         if self.settings.os == "Macos":
             raise ConanInvalidConfiguration("Macos is not supported")
         if self.settings.compiler == "Visual Studio":
-            raise ConanInvalidConfiguration("Building with Visual Studio is not supported")
+            raise ConanInvalidConfiguration("Building with Visual Studio is not supported (due to missing gettimeofday implementation)")
 
     def build_requirements(self):
-        if not tools.which("pkg-config"):
-            self.build_requires("pkgconf/1.7.3")
+        self.build_requires("pkgconf/1.7.3")
 
-    def _patch_cmakelists(self, subfolder):
-        cmakelists_path = os.path.join(self._source_subfolder, subfolder, "CMakeLists.txt")
-        tools.replace_in_file(cmakelists_path, "CMAKE_SOURCE_DIR", "PROJECT_SOURCE_DIR", strict=False)
-        tools.replace_in_file(cmakelists_path, "CMAKE_BINARY_DIR", "PROJECT_BINARY_DIR", strict=False)
+    def requirements(self):
+        self.requires("boost/1.75.0")
+        self.requires("libusb/1.0.23")
 
     def _configure_cmake(self):
         if self._cmake:
             return self._cmake
-
         self._cmake = CMake(self)
-        options = {
+        self._cmake.definitions.update({
             "BUILD_TESTS": False,
             "EXAMPLES": False,
             "FTDI_EEPROM": False,
-            "FTDIPP" : True,
-            "STATICLIBS": not self.options.shared
-        }
-        self._cmake.definitions.update(options)
+            "FTDIPP": True,
+            "STATICLIBS": not self.options.shared,
+        })
         self._cmake.configure()
         return self._cmake
 
-
     def build(self):
-        self._patch_cmakelists("")
-        self._patch_cmakelists("ftdipp")
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
         cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="COPYING*", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
         lib_folder = os.path.join(self.package_folder, "lib",)
         tools.rmdir(os.path.join(lib_folder, "cmake"))
         tools.rmdir(os.path.join(lib_folder, "pkgconfig"))
-        # STATIC config creates both static and shared libraries
-        if not self.options.shared:
-            tools.remove_files_by_mask(lib_folder, "*.so*")
 
         os.unlink(os.path.join(self.package_folder, "bin", "libftdi1-config"))
 
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "LibFTDI1"
         self.cpp_info.names["cmake_find_package_multi"] = "LibFTDI1"
-        self.cpp_info.names["pkgconfig"] = "libftdi1"
 
-        self.cpp_info.components["ftdi"].names["cmake"] = "libftdi"
+        self.cpp_info.components["ftdi"].names["pkg_config"] = "libftdi1"
         self.cpp_info.components["ftdi"].libs = ["ftdi1"]
         self.cpp_info.components["ftdi"].requires = ["libusb::libusb"]
         self.cpp_info.components["ftdi"].includedirs.append(os.path.join("include", "libftdi1"))
 
-        self.cpp_info.components["ftdipp"].names["cmake"] = "libftdipp"
+        self.cpp_info.components["ftdi"].names["pkg_config"] = "libftdi1pp"
         self.cpp_info.components["ftdipp"].libs = ["ftdipp1"]
         self.cpp_info.components["ftdipp"].requires = ["ftdi", "boost::boost"]
         self.cpp_info.components["ftdipp"].includedirs.append(os.path.join("include", "libftdipp1"))
