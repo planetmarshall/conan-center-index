f8541e9f2d804ccbfc61308eb6fa101f544bdfdd
Marshall, Andrew (Senior C++ developer - Sky) <andrew.marshall3@sky.uk>
2026-05-13 16:26:25 +0100

    add launchdarkly 3.11.1 (#111)
    ### Summary
    Changes to recipe:  **launchdarkly/3.11.1**
    
    #### Motivation
    Add latest version of the
    [launchdarkly](https://github.com/launchdarkly) C++ client SDK which has
    fixes to support the latest Boost.
    
    #### Details
    Collapsed the various patches into a single file. The patch 
    * Removes calls to
    [FetchContent](https://cmake.org/cmake/help/latest/module/FetchContent.html)
    as dependencies are provided by conan
    * Disables the Server SDK which we don't need to build.
    
    
    ---
    - [ ] Read the [contributing
    guidelines](https://github.com/conan-io/conan-center-index/blob/master/CONTRIBUTING.md)
    - [ ] Checked that this PR is not a duplicate: [list of PRs by
    recipe](https://github.com/conan-io/conan-center-index/discussions/24240)
    - [ ] If this is a bug fix, please link related issue or provide bug
    details
    - [ ] Tested locally with at least one configuration using a recent
    version of Conan
    
    ---
    Add a :+1: reaction to pull requests you find
    [important](https://github.com/conan-io/conan-center-index/pulls?q=is%3Aopen+sort%3Areactions-%2B1-desc)
    to help the team prioritize, thanks!

--- recipes/launchdarkly/all/conanfile.py
+++ recipes/launchdarkly/all/conanfile.py
@@ -58,7 +58,7 @@ class LaunchDarklyConan(ConanFile):
         self.requires("boost/[>=1.81.0 <2]", force=True, transitive_headers=True)
         self.requires("certify/cci.20201114")
         self.requires("openssl/[>=3 <4]", transitive_headers=True)
-        self.requires("tl-expected/1.1.0", transitive_headers=True)
+        self.requires("tl-expected/[>=1.1.0 <2]", transitive_headers=True)
 
     def validate(self):
         if self.settings.compiler.cppstd:
